ac066293945fde119dd66f2ec3c0e23a20242cd9
mesteves9 <40825315+mesteves9@users.noreply.github.com>
2019-12-06 09:56:19 +0000

    AD Ficha 7 + PPT Aula PL10

---
+++ dim-tables_medical-costs.sql
@@ -0,0 +1,13 @@
+# dim_sex
+INSERT INTO dim_sex (id_sex, sex) VALUES (1, "Female");
+INSERT INTO dim_sex (id_sex, sex) VALUES (2, "Male");
+
+# dim_region
+INSERT INTO dim_region (id_region, region) VALUES (1, "Southwest");
+INSERT INTO dim_region (id_region, region) VALUES (2, "Northwest");
+INSERT INTO dim_region (id_region, region) VALUES (3, "Southeast");
+INSERT INTO dim_region (id_region, region) VALUES (4, "Northeast");
+
+# dim_smoker
+INSERT INTO dim_smoker (id_smoker, smoker) VALUES (1, "Yes");
+INSERT INTO dim_smoker (id_smoker, smoker) VALUES (2, "No");
